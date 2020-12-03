a76bcd6e3ca8e7b185f3115eaf565e4887d33a58
Christina Lucas <christinalucas@Christinas-MacBook-Pro.local>
2020-12-02 21:00:18 -0500

    add projects.md

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 export default [{"title":"About", "fname":"about.md", "specialImage":"/img/menagerie.png"},
 {"title":"index", "fname":"index.md"},
-{"title":"Projects", "fname":"projects.md"}];
+{"title":"Projects", "fname":"projects.md"},
 {"title":"Contact", "fname":"contact.md"}];
