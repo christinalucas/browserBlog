5880707be8785ee29a6873ddd37405235fdd0d2f
Christina Lucas <christinalucas@Christinas-MacBook-Pro.local>
2020-12-06 14:53:27 -0500

    adding about section

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
-export default [{"title":"About", "fname":"about.md", "specialImage":"/img/face.png"},
+export default [{"title":"About", "fname":"about.md"},
 {"title":"index", "fname":"index.md"},
 {"title":"Contact", "fname":"contact.md"}];
